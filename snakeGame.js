c033f61da7c6ba98bf2f37a81bd95bb98554ee00
Lolow63 <laurentbrugiere@yahoo.fr>
2023-12-16 15:20:24 +1100

    Add player's input

--- snakeGame.js
+++ snakeGame.js
@@ -2,13 +2,39 @@
 let snakeElements = [{ x: 10, y: 10 }, { x: 11, y: 10 }, { x: 12, y: 10 }, { x: 12, y: 9 }];
 let food = [{ x: 3, y: 3 }];
 const board = document.getElementById("board");
-
+// loop variables
+let animationFrameId;
+let lastTime = 0;
+let intervalInMilliseconds = 1000;
+let count = 0;
+//player's inputs
+let lastKeyPressed;
 
 // Start the game
 const Start = () => {
     const splashScreen = document.getElementById("splashScreen");
     splashScreen.style.display = "none";
+    document.removeEventListener('keydown', Start);
+    // Start the loop
+    window.requestAnimationFrame(loop);
 };
+document.addEventListener('keydown', Start);
+
+
+function loop(timestamp) {
+    if (timestamp - lastTime >= intervalInMilliseconds) {
+        lastTime = timestamp;
+        // Debug
+        count += 1;
+        console.log(count);
+        console.log('lastKeyPressed:', lastKeyPressed);
+
+        // Run your callback function here
+
+    }
+    // Continue the loop
+    animationFrameId = requestAnimationFrame(loop);
+}
 
 const Draw = () => {
     createSnake(snakeElements);
@@ -22,35 +48,45 @@ const createSnake = (snakeElements) => {
         board.append(snake);
         console.log(snake);
     });
-
 }
-document.addEventListener('keydown', function (event) {
-    Start();
-    if (event.key === 'Escape') {
-        cancelAnimationFrame(animationFrameId);
+
+//listening for player's input
+document.addEventListener("keydown", (event) => {
+    if (event.defaultPrevented) {
+        return; // Do nothing if the event was already processed
     }
-});
 
-const element = document.getElementById("title");
-let animationFrameId;
-let lastTime = 0;
-let intervalInMilliseconds = 1000;
-let count = 0;
-
-function loop(timestamp) {
-    // Check if the specified interval has passed
-    if (timestamp - lastTime >= intervalInMilliseconds) {
-        // Update the lastTime to the current timestamp
-        lastTime = timestamp;
-        count += 1;
-        console.log(count);
-        // Run your callback function here
-        //callbackFunction();
-        element.style.transform = "translateX(10px)";
+    switch (event.key) {
+        case "ArrowDown":
+            console.log("ArrowDown");
+            lastKeyPressed = "ArrowDown"
+            break;
+        case "ArrowUp":
+            console.log("ArrowUp");
+            lastKeyPressed = "ArrowUp"
+            break;
+        case "ArrowLeft":
+            console.log("ArrowLeft");
+            lastKeyPressed = "ArrowLeft"
+            break;
+        case "ArrowRight":
+            console.log("ArrowRight");
+            lastKeyPressed = "ArrowRight"
+            break;
+        case "Enter":
+            // Do something for "enter" or "return" key press.
+            break;
+        case " ":
+            // Do something for "space" key press.
+            break;
+        case "Escape":
+            cancelAnimationFrame(animationFrameId);
+            break;
+        default:
+            return; // Quit when this doesn't handle the key event.
     }
-    // Continue the loop
-    animationFrameId = requestAnimationFrame(loop);
-
-}
-// Start the loop
-window.requestAnimationFrame(loop);
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+},
+    true,
+);
