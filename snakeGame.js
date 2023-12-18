0e95f529847a0072fb9ab99396da6d0bb2d27fc2
Lolow63 <laurentbrugiere@yahoo.fr>
2023-12-18 23:16:03 +1100

    Improved functions naming and fixed an issue with Escape when used to start the game

--- snakeGame.js
+++ snakeGame.js
@@ -1,105 +1,106 @@
-// Global variables
-let snakeElements = [{ row: 10, col: 6 }, { row: 10, col: 5 }, { row: 10, col: 4 }, { row: 10, col: 3 }];
+// Game variables initialisation
+let snakeSegments = [{ row: 10, col: 6 }, { row: 10, col: 5 }, { row: 10, col: 4 }, { row: 10, col: 3 }];
 let foodPosition = { row: 10, col: 15 };
 let isBodyBitten = false;
 let isEaten = false;
 let isWallHit = false;
 let isCellTaken = false;
-const board = document.getElementById("board");
-// loop variables
+let isGameStarted = false;
+let isGamePaused = false;
+
+const gameBoardElement = document.getElementById("gameBoard");
+const splashScreenElement = document.getElementById("splashScreen");
+
+// gameLoop variables initialisation
 let animationFrameId;
-let lastTime = 0;
-let intervalInMilliseconds = 150;
-let count = 0;
-let isPaused = false;
-//player's inputs
+let lastTimestampChecked = 0;
+let snakeSpeed = 150;
+
+//player's inputs initialisation
 let lastKeyPressed = 'ArrowRight';
 let oldKey = 'ArrowRight';
-// Start the game
-const start = () => {
-    const splashScreen = document.getElementById("splashScreen");
-    splashScreen.style.display = "none";
-    document.removeEventListener('keydown', start);
-    drawSnake(snakeElements);
-    drawFood(foodPosition);
-    // Start the loop
-    window.requestAnimationFrame(loop);
+
+// Launch the game
+const startGame = () => {
+    splashScreenElement.style.display = "none";
+    renderSnakeOnBoard(snakeSegments);
+    renderFoodOnBoard(foodPosition);
+    // Start the gameLoop
+    window.requestAnimationFrame(gameLoop);
 };
-document.addEventListener('keydown', start);
+
 // Update the game
-const loop = (timestamp) => {
-    if (timestamp - lastTime >= intervalInMilliseconds) {
-        lastTime = timestamp;
-        // Debug
-        count += 1;
-        console.log(count);
-        checkForCollisions(snakeElements, foodPosition);
-        updateSnake(snakeElements);
+const gameLoop = (timestamp) => {
+    if (timestamp - lastTimestampChecked >= snakeSpeed) {
+        detectCollisions(snakeSegments, foodPosition);
+        updateSnake(snakeSegments);
         while (isCellTaken) {
             updateFood(foodPosition);
-            checkForCollisions(snakeElements, foodPosition);
+            detectCollisions(snakeSegments, foodPosition);
         }
-        if (isWallHit) return end();
-        if (isBodyBitten) return end();
+        if (isWallHit) return endGame();
+        if (isBodyBitten) return endGame();
         // breadcrumbs 
+        lastTimestampChecked = timestamp;
         oldKey = lastKeyPressed;
     };
-    // Continue the loop
-    animationFrameId = requestAnimationFrame(loop);
+    // Continue the gameLoop
+    animationFrameId = requestAnimationFrame(gameLoop);
 };
+
 // end the game
-const end = () => {
+const endGame = () => {
     cancelAnimationFrame(animationFrameId);
     document.removeEventListener('keydown', onPlay);
     alert("game over!")
-}
-const drawSnake = (elements) => {
+};
+
+// high level functions
+const renderSnakeOnBoard = (elements) => {
     elements.forEach(position => {
         let snake = document.createElement("div");
         snake.setAttribute("class", "snake");
         snake.style.gridArea = `${position.row}/${position.col}`;
-        board.append(snake);
+        gameBoardElement.append(snake);
     });
 };
-const drawFood = (position) => {
+const renderFoodOnBoard = (position) => {
     let food = document.createElement("div");
     food.setAttribute("id", "food");
     food.style.gridArea = `${position.row}/${position.col}`;
-    board.append(food);
+    gameBoardElement.append(food);
 };
 const updateSnake = (elements) => {
-    updateSnakePosition(elements);
-    updateSnakeSize(elements);
     const oldSnake = document.querySelectorAll(".snake");
     oldSnake.forEach((element) => element.remove());
-    drawSnake(elements);
+    moveSnake(elements);
+    shrinkSnakeIfNotEaten(elements);
+    renderSnakeOnBoard(elements);
 };
-const checkForCollisions = (player, target) => {
-    checkForBodyBitten(player) >= 2 ? isBodyBitten = true : isBodyBitten = false;
-    checkForFoodEaten(player, target) ? isEaten = true : isEaten = false;
-
-    const checkForWallReached = (element) => element.row > 20 || element.col > 20 || element.row < 0 || element.col < 0;
-    player.some(checkForWallReached) ? isWallHit = true : isWallHit = false;
-
-    const checkForMatchingCell = (element) => element.row === target.row && element.col === target.col;
-    player.some(checkForMatchingCell) ? isCellTaken = true : isCellTaken = false;
-};
-
 const updateFood = (position) => {
-    updateFoodPosition(position);
-    const oldFood = document.getElementById("food");
-    if (oldFood) {
-        oldFood.remove();
-    };
-    drawFood(position);
+    document.getElementById("food").remove();
+    generateRandomFoodPosition(position);
+    renderFoodOnBoard(position);
 };
-const updateSnakePosition = (elements) => {
+const detectCollisions = (snake, target) => {
+    detectBodyBitten(snake) >= 2 ? isBodyBitten = true : isBodyBitten = false;
+    detectFoodEaten(snake, target) ? isEaten = true : isEaten = false;
+
+    const detectWallReached = (element) => element.row > 20 || element.col > 20 || element.row < 0 || element.col < 0;
+    snake.some(detectWallReached) ? isWallHit = true : isWallHit = false;
+
+    const detectMatchingCell = (element) => element.row === target.row && element.col === target.col;
+    snake.some(detectMatchingCell) ? isCellTaken = true : isCellTaken = false;
+};
+
+// Mid level functions
+const moveSnake = (elements) => {
     // cancel backward movement
     if (oldKey === "ArrowDown" && lastKeyPressed === "ArrowUp") lastKeyPressed = oldKey;
     else if (oldKey === "ArrowUp" && lastKeyPressed === "ArrowDown") lastKeyPressed = oldKey;
     else if (oldKey === "ArrowLeft" && lastKeyPressed === "ArrowRight") lastKeyPressed = oldKey;
     else if (oldKey === "ArrowRight" && lastKeyPressed === "ArrowLeft") lastKeyPressed = oldKey;
-    // add a new element at the begining of the snakeElements and remove the last one
+    // add a new segment at the begining of the snakeSegments
     let head = elements[0];
     switch (lastKeyPressed) {
         case "ArrowDown":
@@ -116,29 +117,38 @@ const updateSnakePosition = (elements) => {
             break;
     };
 };
-const updateSnakeSize = (elements) => {
+const shrinkSnakeIfNotEaten = (elements) => {
     if (!isEaten) elements.pop();
 }
-const updateFoodPosition = (position) => {
+const generateRandomFoodPosition = (position) => {
     position.row = Math.floor(Math.random() * 20) + 1;
     position.col = Math.floor(Math.random() * 20) + 1;
 };
-const checkForBodyBitten = (player) => {
+
+// Low level functions
+const detectBodyBitten = (snake) => {
     let duplicate = 0;
     const checkForDuplicate = (element) => {
-        if (player[0].row === element.row && player[0].col === element.col) duplicate += 1;
+        if (snake[0].row === element.row && snake[0].col === element.col) duplicate += 1;
     };
-    player.forEach(checkForDuplicate);
+    snake.forEach(checkForDuplicate);
     return duplicate;
 };
-const checkForFoodEaten = (player, target) => player[0].row === target.row && player[0].col === target.col;
+const detectFoodEaten = (snake, target) => snake[0].row === target.row && snake[0].col === target.col;
 
 // listening for player's input
-
 const onPlay = (event) => {
     if (event.defaultPrevented) {
         return; // Do nothing if the event was already processed
     };
+    // start the game using any keys but escape
+    if (!isGameStarted) {
+        if (event.key !== "Escape") {
+            isGameStarted = true;
+            startGame();
+        }
+    }
+    // control handling
     switch (event.key) {
         case "ArrowDown":
             lastKeyPressed = "ArrowDown";
@@ -159,9 +169,19 @@ const onPlay = (event) => {
             // Do something for "space" key press.
             break;
         case "Escape":
-            isPaused = !isPaused;
-            if (isPaused) cancelAnimationFrame(animationFrameId);
-            else if (!isPaused) requestAnimationFrame(loop);
+            if (isGameStarted) {
+                // use Escape to toggle between pause and start
+                isGamePaused = !isGamePaused;
+                if (isGamePaused) {
+                    cancelAnimationFrame(animationFrameId);
+                } else {
+                    requestAnimationFrame(gameLoop);
+                }
+            } else {
+                // start the game using Escape
+                isGameStarted = true;
+                startGame();
+            }
             break;
         default:
             return; // Quit when this doesn't handle the key event.
@@ -169,4 +189,4 @@ const onPlay = (event) => {
     // Cancel the default action to avoid it being handled twice
     event.preventDefault();
 };
-document.addEventListener("keydown", onPlay, true)
+document.addEventListener("keydown", onPlay, true);
